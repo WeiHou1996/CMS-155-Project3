e9920c7c4f9bfa622a1a1a08334531437c80b56a
Michael Sleeman <michael.k.sleeman@gmail.com>
2024-03-03 12:33:06 -0800

    Added script to pre-process data

---
+++ unsupervised_HMM.py
@@ -0,0 +1,53 @@
+import numpy as np
+from HiddenMarkovModel import HiddenMarkovModel
+
+def unsupervised_HMM(X, n_states, N_iters, seed=None):
+    '''
+    Helper function to train an unsupervised HMM. The function determines the
+    number of unique observations in the given data, initializes
+    the transition and observation matrices, creates the HMM, and then runs
+    the training function for unsupervised learing.
+    Arguments:
+        X:          A dataset consisting of input sequences in the form
+                    of lists of variable length, consisting of integers
+                    ranging from 0 to D - 1. In other words, a list of lists.
+        n_states:   Number of hidden states to use in training.
+
+        N_iters:    The number of iterations to train on.
+        rng:        The random number generator for reproducible result.
+                    Default to RandomState(1).
+    '''
+    # Initialize random number generator
+    rng = np.random.default_rng(seed=seed)
+
+    # Make a set of observations.
+    observations = set()
+    for x in X:
+        observations |= set(x)
+
+    # Compute L and D.
+    L = n_states
+    D = len(observations)
+
+    # Randomly initialize and normalize matrix A.
+    A = [[rng.random() for i in range(L)] for j in range(L)]
+
+    for i in range(len(A)):
+        norm = sum(A[i])
+        for j in range(len(A[i])):
+            A[i][j] /= norm
+
+    # Randomly initialize and normalize matrix O.
+    O = [[rng.random() for i in range(D)] for j in range(L)]
+
+    for i in range(len(O)):
+        norm = sum(O[i])
+        for j in range(len(O[i])):
+            O[i][j] /= norm
+
+    # Train an HMM with unlabeled data.
+    HMM = HiddenMarkovModel(A, O)
+    HMM.unsupervised_learning(X, N_iters)
+
+    return HMM
+    
