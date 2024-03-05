2812fc06ae2b1e729e3fae715051234f9254bc9e
Michael Sleeman <michael.k.sleeman@gmail.com>
2024-03-04 16:11:32 -0800

    Updated how dictionary is read

--- syllable_reader_2.py
+++ syllable_reader_2.py
@@ -3,26 +3,15 @@ def syllable_read(file):
     syl = {}
 
     for x in syllable:
-        w = x.split(" ")
-        name = (w[0]).replace("\n", "")
-        tmp = ["" for _ in range(2)]
-        for j in range(1, len(w)):
-            if len(w) > 2:
-                if w[1][0] == "E":
-                    tmp[0] = (w[2]).replace("\n", "")
-                    tmp[1] = (w[1]).replace("E", "")
-                elif w[2][0] == "E":
-                    tmp[0] = (w[1]).replace("\n", "").replace("E", "")
-                    tmp[1] = w[2]
-                else:
-                    tmp[0] = (w[1]).replace("E", "")
-                    tmp[1] = (w[2]).replace("\n", "")
-            else:
-                tmp[0] = (w[1]).replace("\n", "")
-                tmp[1] = (w[1]).replace("\n", "")
-
-            syl[name] = tmp
+        line = x.split(" ")
+        word = (line[0]).replace("\n", "")
+        count = []
+        for j in range(1, len(line)):
+            thisCount = line[j].replace("\n","")
+            count.append(thisCount)
+        
+        syl[word] = count
 
     syllable.close()
-
+    
     return syl
